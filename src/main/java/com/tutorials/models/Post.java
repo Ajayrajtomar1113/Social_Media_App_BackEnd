ce141632fbfa8f7d610f835141e176088c097b01
Ajayrajtomar1113 <ajayrajtomar.143@gmail.com>
2026-04-14 14:06:32 +0530

    improve post and user entity

--- src/main/java/com/tutorials/models/Post.java
+++ src/main/java/com/tutorials/models/Post.java
@@ -1,3 +1,76 @@
+//
+//
+//package com.tutorials.models;
+//
+//import java.time.LocalDateTime;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import org.hibernate.annotations.CreationTimestamp;
+//
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//
+//import jakarta.persistence.*;
+//
+//@Entity
+//public class Post {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Integer id;
+//
+//    private String caption;
+//    private String image;
+//    private String video;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "user_id")
+//    @JsonIgnoreProperties({"savedPost","followers","followings","password","posts","reels"})
+//    private User user;
+//
+//    @ManyToMany
+//    @JoinTable(
+//        name = "post_likes",
+//        joinColumns = @JoinColumn(name = "post_id"),
+//        inverseJoinColumns = @JoinColumn(name = "user_id")
+//    )
+//    private List<User> liked = new ArrayList<>();
+//
+//    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+//    @JsonIgnoreProperties("post")
+//    private List<Comment> comments = new ArrayList<>();
+//
+//    @CreationTimestamp
+//    private LocalDateTime createdAt;
+//
+//    public Post() {}
+//
+//    // GETTERS & SETTERS
+//
+//    public Integer getId() { return id; }
+//    public void setId(Integer id) { this.id = id; }
+//
+//    public String getCaption() { return caption; }
+//    public void setCaption(String caption) { this.caption = caption; }
+//
+//    public String getImage() { return image; }
+//    public void setImage(String image) { this.image = image; }
+//
+//    public String getVideo() { return video; }
+//    public void setVideo(String video) { this.video = video; }
+//
+//    public User getUser() { return user; }
+//    public void setUser(User user) { this.user = user; }
+//
+//    public List<User> getLiked() { return liked; }
+//    public void setLiked(List<User> liked) { this.liked = liked; }
+//
+//    public List<Comment> getComments() { return comments; }
+//    public void setComments(List<Comment> comments) { this.comments = comments; }
+//
+//    public LocalDateTime getCreatedAt() { return createdAt; }
+//    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+//}
 
 
 package com.tutorials.models;
@@ -23,9 +96,17 @@ public class Post {
     private String image;
     private String video;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
-    @JsonIgnoreProperties({"savedPost","followers","followings","password","posts","reels"})
+    @JsonIgnoreProperties({
+        "savedPost",
+        "followers",
+        "followings",
+        "password",
+        "posts",
+        "reels",
+        "liked"
+    })
     private User user;
 
     @ManyToMany
@@ -34,6 +115,15 @@ public class Post {
         joinColumns = @JoinColumn(name = "post_id"),
         inverseJoinColumns = @JoinColumn(name = "user_id")
     )
+    @JsonIgnoreProperties({
+        "savedPost",
+        "followers",
+        "followings",
+        "password",
+        "posts",
+        "reels",
+        "liked"
+    })
     private List<User> liked = new ArrayList<>();
 
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
@@ -45,29 +135,68 @@ public class Post {
 
     public Post() {}
 
-    // GETTERS & SETTERS
 
-    public Integer getId() { return id; }
-    public void setId(Integer id) { this.id = id; }
+    public Integer getId() {
+        return id;
+    }
 
-    public String getCaption() { return caption; }
-    public void setCaption(String caption) { this.caption = caption; }
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-    public String getImage() { return image; }
-    public void setImage(String image) { this.image = image; }
+    public String getCaption() {
+        return caption;
+    }
 
-    public String getVideo() { return video; }
-    public void setVideo(String video) { this.video = video; }
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
 
-    public User getUser() { return user; }
-    public void setUser(User user) { this.user = user; }
+    public String getImage() {
+        return image;
+    }
 
-    public List<User> getLiked() { return liked; }
-    public void setLiked(List<User> liked) { this.liked = liked; }
+    public void setImage(String image) {
+        this.image = image;
+    }
 
-    public List<Comment> getComments() { return comments; }
-    public void setComments(List<Comment> comments) { this.comments = comments; }
+    public String getVideo() {
+        return video;
+    }
 
-    public LocalDateTime getCreatedAt() { return createdAt; }
-    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+    public void setVideo(String video) {
+        this.video = video;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<User> getLiked() {
+        return liked;
+    }
+
+    public void setLiked(List<User> liked) {
+        this.liked = liked;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
--- src/main/java/com/tutorials/models/User.java
+++ src/main/java/com/tutorials/models/User.java
@@ -1,9 +1,99 @@
+//
+//package com.tutorials.models;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import com.fasterxml.jackson.annotation.JsonProperty;
+//
+//import jakarta.persistence.*;
+//
+//@Entity
+//@Table(name = "users")
+//public class User {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Integer id;
+//
+//    private String firstName;
+//    private String lastName;
+//
+//    @Column(unique = true)
+//    private String email;
+//
+//    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+//    private String password;
+//
+//    private String gender;
+//
+//    @Enumerated(EnumType.STRING)
+//    private Role role;
+//
+//    @ManyToMany
+//    private List<Post> savedPost = new ArrayList<>();
+//
+//    @ElementCollection
+//    private List<Integer> followers = new ArrayList<>();
+//
+//    @ElementCollection
+//    private List<Integer> followings = new ArrayList<>();
+//
+//    // 🔥 IMPORTANT RELATIONS
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<Post> posts = new ArrayList<>();
+//
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<Reels> reels = new ArrayList<>();
+//
+//    public User() {}
+//
+//    // GETTERS & SETTERS
+//
+//    public Integer getId() { return id; }
+//    public void setId(Integer id) { this.id = id; }
+//
+//    public String getFirstName() { return firstName; }
+//    public void setFirstName(String firstName) { this.firstName = firstName; }
+//
+//    public String getLastName() { return lastName; }
+//    public void setLastName(String lastName) { this.lastName = lastName; }
+//
+//    public String getEmail() { return email; }
+//    public void setEmail(String email) { this.email = email; }
+//
+//    public String getPassword() { return password; }
+//    public void setPassword(String password) { this.password = password; }
+//
+//    public String getGender() { return gender; }
+//    public void setGender(String gender) { this.gender = gender; }
+//
+//    public Role getRole() { return role; }
+//    public void setRole(Role role) { this.role = role; }
+//
+//    public List<Post> getSavedPost() { return savedPost; }
+//    public void setSavedPost(List<Post> savedPost) { this.savedPost = savedPost; }
+//
+//    public List<Integer> getFollowers() { return followers; }
+//    public void setFollowers(List<Integer> followers) { this.followers = followers; }
+//
+//    public List<Integer> getFollowings() { return followings; }
+//    public void setFollowings(List<Integer> followings) { this.followings = followings; }
+//
+//    public List<Post> getPosts() { return posts; }
+//    public void setPosts(List<Post> posts) { this.posts = posts; }
+//
+//    public List<Reels> getReels() { return reels; }
+//    public void setReels(List<Reels> reels) { this.reels = reels; }
+//}
+
 
 package com.tutorials.models;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.*;
@@ -30,25 +120,41 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    /* =========================
+       SAVED POSTS
+    ========================= */
     @ManyToMany
+    @JsonIgnore   // 🔥 IMPORTANT
     private List<Post> savedPost = new ArrayList<>();
 
+    /* =========================
+       FOLLOW SYSTEM
+    ========================= */
     @ElementCollection
     private List<Integer> followers = new ArrayList<>();
 
     @ElementCollection
     private List<Integer> followings = new ArrayList<>();
 
-    // 🔥 IMPORTANT RELATIONS
+    /* =========================
+       USER POSTS
+    ========================= */
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore   // 🔥 MOST IMPORTANT (loop break)
     private List<Post> posts = new ArrayList<>();
 
+    /* =========================
+       USER REELS
+    ========================= */
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
     private List<Reels> reels = new ArrayList<>();
 
     public User() {}
 
-    // GETTERS & SETTERS
+    /* =========================
+       GETTERS & SETTERS
+    ========================= */
 
     public Integer getId() { return id; }
     public void setId(Integer id) { this.id = id; }
